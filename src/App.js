1faf02636b7e01d88e2871560d0e892917dc9982
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2023-10-31 12:41:55 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,275 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Webcam from "react-webcam";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Amplify, API} from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
+
+
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [location, setLocation] = useState(null);
+  const [labels, setLabels] = useState([]);
+  const [image, setImage] = useState(null);
+  const [capturing, setCapturing] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [isCapturing, setIsCapturing] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const [resp, setResp] = React.useState("")
+
+  useEffect(() =>
+  {
+    const apiName = 'imageUpload';
+    const path = '/uploader';
+    API.post(apiName, path).then((res) => console.log(res))
+
+
+  },[])
+  // Define a state variable to keep track of the last uploaded image key
+
+  const apiBaseUrl = process.env.REACT_APP_API_URL  || 'http://3.85.16.0:5000/upload';
+
+// Now, you can use the apiBaseUrl variable in your component
+
+
+async function getData() {
+  const apiName = 'imageUpload';
+  const path = '/uploader';
+  const myInit = {
+    headers: {} // OPTIONAL
+  };
+
+  return API.get(apiName, path, myInit);
 }
 
-export default App;
+
+// try {
+
+//   (async function() {
+//     const response = await getData();
+//     console.log(response)
+//   })();
+  
+// } catch (error) {
+
+//   console.log(error)
+  
+// }
+
+
+  const captureImage = async () => {
+    setCapturing(true);
+    // Capture logic here
+    setCapturing(false); // Reset capturing state when done
+  };
+
+  const handleUploadImage = async () => {
+    setUploading(true);
+    // Upload logic here
+    setUploading(false); // Reset uploading state when done
+  };
+
+  async function sendImageToServer(data) {
+
+      
+    fetch(apiBaseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+})
+
+  }
+
+  
+  const handleUpload = () => {
+
+    console.log( {base64Data: image});
+    const data = {base64Data: image}
+    
+    fetch('http://3.85.16.0:5000/upload', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+  .then((data) => {
+  if (data.success) {
+    console.log( data.labels,
+      setLabels(data.labels));
+  } else {
+    console.error('Upload failed: ' + data.message);
+  }})
+  .catch((error) => {
+    console.error('An error occurred:', error);});
+
+
+  }
+
+  function hashString(text) {
+    const encoder = new TextEncoder();
+    const encodedText = encoder.encode(text);
+    const hash = new Uint8Array(32);
+    crypto.subtle.digest("SHA-256", encodedText, hash);
+    return hash.join("");
+  }
+  
+  
+  const captureNew = async () => {
+    setIsCapturing(true); // Set isCapturing to true while capturing
+
+    const screenshot = webcamRef.current.getScreenshot();
+
+    if (screenshot) {
+      setImage(screenshot);
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          latitude: hashString(position.coords.latitude).splice(0,5) ,
+          longitude: hashString(position.coords.longitude).splice(0,5),
+        });
+      });
+    } else {
+      console.log("Invalid image format. Please capture a PNG or JPEG image.");
+    }
+
+    setIsCapturing(false); // Reset isCapturing when done capturing
+  };
+
+
+
+  const handleUploadNew = () => {
+    setIsUploading(true); // Set isUploading to true while uploading
+
+     console.log( {base64Data: image});
+    const data = {base64Data: image, coordinates: JSON.stringify(location)}
+    
+    fetch('https://2445-2603-8080-6300-b84-f135-a88c-f93a-f678.ngrok.io/upload', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+  .then((data) => {
+  if (data.success) {
+    console.log( data.labels,
+      setLabels(data.labels));
+  } else {
+    console.error('Upload failed: ' + data.message);
+  }})
+  .catch((error) => {
+    console.error('An error occurred:', error);});
+
+    setIsUploading(false); // Reset isUploading when done uploading
+  };
+
+  const capture = async () => {
+    const screenshot = webcamRef.current.getScreenshot();
+ 
+
+    if (screenshot) {
+      setImage(screenshot)
+
+      setImages([...images, screenshot]);
+
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+
+      
+    } else {
+      console.log("Invalid image format. Please capture a PNG or JPEG image.");
+    }
+  };
+
+ 
+
+  const webcamRef = React.useRef(null);
+
+  return (
+    <div className="app-container">
+    <h3>Image </h3>
+    <h3>Classification </h3>
+    <div className="container">
+      <div className="webcam-container">
+        <Webcam
+          audio={false}
+          screenshotFormat="image/jpeg"
+          height={200}
+          width={200}
+          ref={webcamRef}
+        />
+
+        <button onClick={captureNew} disabled={isCapturing || isUploading}>
+          Capture
+        </button>
+        <button onClick={handleUploadNew} disabled={isCapturing || isUploading}>
+          Upload
+        </button>
+      </div>
+    </div>
+
+        <div className="image-container">
+            {/* Display the last captured image */}
+            {/* {isCapturing ? (
+          // Show a spinner while capturing
+          <div className="spinner"></div>
+        ) : (
+          // Display the last captured image when not capturing
+          image && (
+            <div className="image-card">
+              <img src={image} alt={`Last Captured`} />
+            </div>
+          )
+        )} */}
+
+        {isUploading && (
+          // Show a spinner while uploading
+          <div className="spinner"></div>
+        )}
+
+            {labels.length > 0 && (
+          <div className="labels">
+            <h2>Labels:</h2>
+            <ul>
+              {labels.map((label, index) => (
+                <li key={index}>{label.Name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+            {/* ... (rest of your code for displaying analysis results) */}
+        </div>
+        {location && (
+          <div className="gps">
+          <p>
+                Latitude: {location.latitude}
+            </p>
+
+            <p>
+             Longitude: {location.longitude}
+            </p>
+          
+          </div>
+           
+            
+        )}
+       
+    </div>
+  );
+};
+export default withAuthenticator(App);
