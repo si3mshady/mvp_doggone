bac717712cfd33786ec108a6b0f29a215f369897
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2023-11-04 10:23:10 -0500

    ready for testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Webcam from "react-webcam";
 import "./App.css";
-import { Amplify, API } from 'aws-amplify';
+import { Amplify, API} from 'aws-amplify';
 import awsmobile from './aws-exports';
 
-Amplify.configure(awsmobile);
+Amplify.configure(
+  awsmobile
+);
+
+
+
 
 const App = () => {
   const [images, setImages] = useState([]);
@@ -15,74 +20,104 @@ const App = () => {
   const [uploading, setUploading] = useState(false);
   const [isCapturing, setIsCapturing] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
-  const [data, setPostData] = useState({ body: {}, headers: {} });
-  const [facingMode, setFacingMode] = useState("user"); // "user" for front camera, "environment" for back camera
+  const [data, setPostData] = useState({body: {}, headers: {}});
 
-  const [resp, setResp] = React.useState("");
 
+  const [resp, setResp] = React.useState("")
+
+  
+  
   const captureNew = async () => {
-    setIsCapturing(true);
+    setIsCapturing(true); // Set isCapturing to true while capturing
 
     const screenshot = webcamRef.current.getScreenshot();
 
     if (screenshot) {
       setImage(screenshot);
 
+     
+
       navigator.geolocation.getCurrentPosition((position) => {
         setLocation({
-          latitude: position.coords.latitude,
+          latitude: position.coords.latitude ,
           longitude: position.coords.longitude,
         });
+
+
+
       });
     } else {
       console.log("Invalid image format. Please capture a PNG or JPEG image.");
     }
 
-    setIsCapturing(false);
+    setIsCapturing(false); // Reset isCapturing when done capturing
   };
 
   const handleUploadNew = () => {
-    setIsUploading(true);
+    console.log("sending data")
+    setIsUploading(true); // Set isUploading to true while uploading
 
-    const data = JSON.stringify({ base64Data: image, coordinates: JSON.stringify(location) });
+     console.log( {base64Data: image});
+    const data = JSON.stringify({base64Data: image, coordinates: JSON.stringify(location)})
 
+    const myInit = {
+      body: {data}, // replace this with attributes you need
+      // headers: {} // OPTIONAL
+    };
+
+    // const b64data = JSON.stringify(screenshot)
+    // const apiName = 'doggonepy3'; // replace this with your api name.
+    // const path = '/upload';
+
+    // API.post(apiName, path, myInit)
+    // .then((response) => {
+    //   // Add your code here
+    //   console.log(response)
+    //   console.log("Data sent")
+    //   setIsUploading(false); // Set
+    // })
+    // .catch((error) => {
+    //   console.log(error.response);
+    // });
+
+
+
+    
     fetch('https://00ngobjr57.execute-api.us-east-1.amazonaws.com/dev/upload', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data) {
-          console.log(data);
-          setLabels(data.labels);
-        } else {
-          console.error('Upload failed: ' + data.message);
-        }
-      })
-      .catch((error) => {
-        console.error('An error occurred:', error);
-      });
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+  .then((data) => {
+  if (data) {
+    console.log( data)
 
-    setIsUploading(false);
+      setLabels(data.labels)
+      // const keys = Object.keys(data);
+
+      console.log( data.labels)
+  } else {
+    console.error('Upload failed: ' + data.message);
+  }})
+  .catch((error) => {
+    console.error('An error occurred:', error);});
+
+    setIsUploading(false); // Reset isUploading when done uploading
   };
+
 
   const webcamRef = React.useRef(null);
 
-  // Function to toggle between the front and back camera
-  const toggleCamera = () => {
-    setFacingMode((prevMode) => (prevMode === "user" ? "environment" : "user"));
-  };
-
   return (
     <div className="app-container">
-      <h3>Image </h3>
-      <h3>Classification </h3>
-      <div className="container">
-        <div className="webcam-container">
-          <Webcam
+    <h3>Image </h3>
+    <h3>Classification </h3>
+    <div className="container">
+      <div className="webcam-container">
+      <Webcam
             audio={false}
             screenshotFormat="image/jpeg"
             height={200}
@@ -93,18 +128,60 @@ const App = () => {
             }}
           />
 
-          <button onClick={captureNew} disabled={isCapturing || isUploading}>
-            Capture
-          </button>
-          <button onClick={handleUploadNew} disabled={isCapturing || isUploading}>
-            Upload
-          </button>
-          <button onClick={toggleCamera} disabled={isCapturing || isUploading}>
+        <button onClick={captureNew} disabled={isCapturing || isUploading}>
+          Capture
+        </button>
+        <button onClick={handleUploadNew} disabled={isCapturing || isUploading}>
+          Upload
+        </button>
+        <button onClick={toggleCamera} disabled={isCapturing || isUploading}>
             Toggle Camera
           </button>
-        </div>
       </div>
-      {/* Rest of your code remains the same */}
+    </div>
+
+        <div className="image-container">
+            {/* Display the last captured image */}
+            {/* {isCapturing ? (
+          // Show a spinner while capturing
+          <div className="spinner"></div>
+        ) : (
+          // Display the last captured image when not capturing
+          image && (
+            <div className="image-card">
+              <img src={image} alt={`Last Captured`} />
+            </div>
+          )
+        )} */}
+
+
+            {labels.length > 0 && (
+          <div className="labels">
+            <h2>Labels:</h2>
+            <ul>
+              {labels.map((label, index) => (
+                <li key={index}>{label.Name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+            {/* ... (rest of your code for displaying analysis results) */}
+        </div>
+        {location && (
+          <div className="gps">
+          <p>
+                Latitude: {location.latitude}
+            </p>
+
+            <p>
+             Longitude: {location.longitude}
+            </p>
+          
+          </div>
+           
+            
+        )}
+       
     </div>
   );
 };
